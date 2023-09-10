aab5551594351a58078c8734e0ec70f6d037a2f5
bochrap <131978926+bochrap@users.noreply.github.com>
2023-09-10 17:18:35 +0100

    add grid layout withcolor change on hover

---
+++ script.js
@@ -0,0 +1,48 @@
+let grid = document.querySelector('#grid');
+
+
+    for (i = 0; i < 19; i++) {
+        let div = document.createElement("div");
+        div.setAttribute("class", "column");
+        grid.appendChild(div);
+    };
+
+    for (i = 0; i < 19; i++) {
+        let elements = document.querySelectorAll(".column");
+        let div = document.createElement("div");
+        
+
+        elements.forEach(element => {
+            div.setAttribute("class", "cell");
+            element.appendChild(div.cloneNode(true));
+        });
+    };
+
+    let cell = document.querySelector('.cell');
+    let cells = document.querySelectorAll('.cell');
+
+    cells.forEach(element => {
+        element.addEventListener("mouseover", (event) => {
+            element.classList.remove("cell");
+            element.classList.add("cell_new");
+        });
+    });
+
+
+
+
+  
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
